413d640e24d6672467079f3ceabd16cb326649a0
dhruvil <dd.droid@yahoo.com>
2016-10-27 20:01:04 +0530

    Initial commit

---
+++ apicalling/src/main/java/com/webmyne/apicalling/MyCustomClassForLIbrary.java
@@ -0,0 +1,13 @@
+package com.webmyne.apicalling;
+
+/**
+ * Created by dhruvil on 27-10-2016.
+ */
+
+public class MyCustomClassForLIbrary {
+
+    public String name;
+    public String email;
+
+
+}
